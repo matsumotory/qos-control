c057ba5845bb473a4c0afccd65fa3e21279d653c
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-06-06 07:59:04 -0700

    Fix init.d path

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ perl -Iextlib/lib/perl5 cpanm -n -L extlib --installdeps .
 
 # copy traffic control scripts
 sudo cp -prv etc/sysconfig/qos /etc/sysconfig/.
-sudo cp -prv etc/rc.d/init.d/qos.init etc/init.d/.
+sudo cp -prv etc/rc.d/init.d/qos.init /etc/rc.d/init.d/.
 sudo cp -prv bin/qos.sh /usr/local/sbin/.
 
 # config ethtool for cbq
